dd3e257ed7feb1b1e91fed0ddb0f5be79d780033
Philipp Lenz <philipp.lenz@hotmail.com>
2021-10-16 00:12:20 +0200

    uploader added

---
+++ src/hackathon_assets/src/router.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import Staker from './staker';
+import Uploader from './uploader';
+import Main from './index';
+
+
+// this is the router
+// import and call routToPage(<name of page(string)>) from any component
+export default function routToPage(pageName) {
+    switch(pageName) {
+        case "Staker":
+            render(<Staker />, document.getElementById("app"));
+            break;
+        case "Uploader":
+            render(<Uploader />, document.getElementById("app"));
+            break;
+        case "Main":
+            render(<Main />, document.getElementById("app"));
+            break;
+      default:
+    }
+  }
