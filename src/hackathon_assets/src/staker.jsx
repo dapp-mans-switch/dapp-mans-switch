4475cd77e89158d9a2bd5511308f3e080e39bda3
Philipp Lenz <philipp.lenz@hotmail.com>
2021-10-15 23:27:19 +0200

    router added and frontend split up

--- src/hackathon_assets/src/staker.jsx
+++ src/hackathon_assets/src/staker.jsx
@@ -1,42 +1,77 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { hackathon } from "../../declarations/hackathon";
+import routToPage from './router';
 
-import { Link } from 'react-router-dom';
-// import Staker from "./staker"; 
-// <Route path="staker" component={Staker} /> 
 
-const Staker = () => {
-  const [name, setName] = React.useState('');
-  const [message, setMessage] = React.useState('');
-  
-  async function doGreet() {
-    const greeting = await hackathon.greet(name);
-    setMessage(greeting);
+export default function Staker() {
+
+  const [amount, setAmount] = React.useState('');
+  const [duration, setDuration] = React.useState('');  
+  const [myStakes, setMyStakes] = React.useState(''); 
+
+
+  async function addStaker() {
+    console.log("Staker request sent");
+    // TODO: replace "Staker1" by identification Auth
+    let a = parseInt(amount);
+    let d = parseInt(duration);
+    if (isNaN(a) || isNaN(d) || a < 0 || a < 0) {
+      alert("amount and duration must be positive integer");
+    } else {
+      const newStakerId = await hackathon.addStaker("Staker1", a, d);
+      alert(newStakerId);
+    }
   }
+
   
+  async function listAllStakers() {
+    let stakes = await hackathon.listAllStakers();
+    
+    var table = document.getElementById("stakerTable");
+    
+    let col_names = ["name", "amount", "days"];
+    table.innerHTML = "";
+
+    var tr = table.insertRow(-1);
+    for (const cn of col_names) {
+      var tabCell = tr.insertCell(-1);
+      tabCell.innerHTML = cn
+    }
+
+
+    stakes.map(function (s) {
+      var tr = table.insertRow(-1);
+      for (const cn of col_names) {
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = s[cn];
+      }
+    });
+
+  }
+
   return (
     <div>
       <h1>Staker</h1>
-      This is the staker Page
-      <Link to="/staker">GO TO STAKER</Link>
-      <a id="staker_button" data-text="Staker" class="rainbow-button" style={{width: 150}}></a>
-      <a id="uploader_button" data-text="Uploader" class="rainbow-button" style={{width: 180}}></a>
+      This is the Staker's page.
 
-    {/* <div style={{ margin: "30px" }}>
-    <input
-    id="name"
-    value={name}
-    onChange={(ev) => setName(ev.target.value)}
-    ></input>
-    <button onClick={doGreet}>Get Greeting!</button>
-    </div>
-    <div>
-    Greeting is: "
-        <span style={{ color: "blue" }}>{message}</span>"
-    </div> */}
+      <form id="staker_form">
+        <label htmlFor="stakeAmount">Amount:</label>
+        <input id="stakeAmount" type="text" onChange={(ev) => setAmount(ev.target.value)}/> <br/>
+        <label htmlFor="stakeDuration">Duration (Days):</label>
+        <input id="stakeDuration" type="text" onChange={(ev) => setDuration(ev.target.value)}/> <br/>
+        <label htmlFor="no_new_stakes">Don't receive new shares</label>
+        <input type="checkbox" id="no_new_stakes" name="no_new_stakes" value="Stakes"/>
+        <a id="add_new_stake_button" data-text="Start Stake" onClick={addStaker} class="rainbow-button" style={{width: 300}}></a>
+        <br/>
+      </form>
+
+      <h2>My Stakes</h2>
+      <button onClick={listAllStakers}>List my Stakes</button>
+
+      <table id="stakerTable" border={2} cellPadding={5}/>
+      
+      <button onClick={() => {routToPage("Main")}}>Back to Start Page</button>
     </div>
   );
 };
-
-render(<Main />, document.getElementById("app"));
