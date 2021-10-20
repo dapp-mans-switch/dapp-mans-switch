22f0d608dcb5f8503a85305f4813b76af441c9b6
Philipp Lenz <philipp.lenz@hotmail.com>
2021-10-20 02:00:59 +0200

    made love to the start page

--- src/hackathon_assets/src/index.jsx
+++ src/hackathon_assets/src/index.jsx
@@ -10,12 +10,20 @@ export default function Main() {
     
       <h1>Dead Man’s Switch</h1>
       <h3>using Proof of Stake</h3>
-      
+
       regularly verify that you are alive, otherwise your uploaded files will get published
 
-      <a id="staker_button" data-text="Staker" onClick={() => routToPage('Staker')} class="rainbow-button" style={{width: 150}}></a>
-      <a id="uploader_button" data-text="Uploader" onClick={() => routToPage('Uploader')} class="rainbow-button" style={{width: 180}}></a>
-      <a id="spectator_button" data-text="Spectator" onClick={() => routToPage('Spectator')} class="rainbow-button" style={{width: 180}}></a>
+      <div class="panel">
+        <video autoPlay loop class="key-flip-video">
+          <source src="/assets/key-flip.mkv"/>
+        </video>
+
+        <div class="start-page-button-div">
+          <a id="staker_button" data-text="Staker" onClick={() => routToPage('Staker')} class="rainbow-button" style={{width: 150}}></a>
+          <a id="uploader_button" data-text="Uploader" onClick={() => routToPage('Uploader')} class="rainbow-button" style={{width: 180}}></a>
+          <a id="spectator_button" data-text="Spectator" onClick={() => routToPage('Spectator')} class="rainbow-button" style={{width: 180}}></a>
+        </div>
+      </div>
 
     </div>
   );
